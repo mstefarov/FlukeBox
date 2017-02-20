5359fe94bb87710a6c9a7aa622ecbae3760c7e35
fragmer <me@matvei.org>
2017-02-19 19:32:59 -0800

    Stubbing out IMetadataRepo

---
+++ FlukeBox/MediaLibrary/IMetadataRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlukeBox.Models;
+
+namespace FlukeBox.MediaLibrary
+{
+    public interface IMetadataRepo
+    {
+        TrackFull GetTrackFull(int id);
+    }
+}
---
+++ FlukeBox/MediaLibrary/SqliteMetadataRepo.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using Dapper;
+using FlukeBox.Models;
+using Microsoft.Data.Sqlite;
+
+namespace FlukeBox.MediaLibrary {
+    internal class SqliteMetadataRepo : IMetadataRepo {
+        private const string ConnectionString = "Filename=./FlukeboxLibrary.db";
+
+        private static IDbConnection GetConnection() {
+            return new SqliteConnection(ConnectionString);
+        }
+
+        public TrackFull GetTrackFull(int id)
+        {
+            using (var connection = GetConnection())
+            {
+                const string query = "SELECT * FROM Tracks WHERE Id = @Id";
+                connection.Open();
+                return connection.QueryFirstOrDefault<TrackFull>(query, new
+                {
+                    Id = id
+                });
+            }
+        }
+    }
+}
