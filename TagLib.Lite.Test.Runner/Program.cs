8dd6d008b4cb52c23e65d90e50fa8b8051f64f61
fragmer <me@matvei.org>
2017-03-18 23:56:00 -0700

    Got unit tests running again
    Turns out dotnet-test-nunit does not work with csproj core projects yet. Had to improvise and use NUnitLite instead. Hopefully ReSharper and VisualStudio make running tests easier in the future. Also:
    - Removed temp files that were accidentally checked in with the test project.
    - Cleaned up leftover junk in csproj files, left over from project migration.

---
+++ TagLib.Lite.Test.Runner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Common;
+using NUnitLite;
+using TagLib.Tests;
+
+namespace TagLib.Lite.Test.Runner
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            int result = new AutoRun(typeof(MySetUpClass).GetTypeInfo().Assembly)
+                .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+            Console.ReadLine();
+            return result;
+        }
+    }
+}
